c65e005167fde8d6f5d73b3d3cfaf8618cf018de
Ashish Kumar <ashishkr23438@gmail.com>
2019-11-08 11:21:19 +0530

    Fixed sslmode not supported

--- swiftrail/settings.py
+++ swiftrail/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
 }
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=600, ssl_require=True))
+del DATABASES['default']['OPTIONS']['sslmode']
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
