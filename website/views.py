9b60ef700cf283aa744f458a9e038f388c47f7d2
mrsac7 <mrsac7@gmail.com>
2019-11-10 00:58:27 +0530

    initial fron-end

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 app_name = 'website/'
-
+acc_name = 'accounts/'
 def index(request):
     return render(request, app_name + 'index.html')
 
@@ -19,3 +19,12 @@ def train_search(request):
 
 def book_ticket(request):
     return render(request, app_name + 'book-ticket.html')
+
+def book_now(request):
+    return render(request, app_name + 'book-now.html')
+
+def transaction_success(request):
+    return render(request, app_name + 'transaction-success.html')
+
+def edit_profile(request):
+    return render(request, acc_name + 'edit-profile.html')
